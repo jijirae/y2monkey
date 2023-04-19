d2729fe21144aea33493b321816d063f61161d08
jiji <122718637+jijirae@users.noreply.github.com>
2023-04-20 04:03:46 +0800

    Changed key combination to CTRL+SHIFT+D
    This is because I kept accidentally downloading the video when typing a comment.

--- y2monkey.user.js
+++ y2monkey.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         y2monkey
 // @namespace    https://github.com/jijirae/y2monkey
-// @version      1.0
-// @description  Pressing "SHIFT+D" opens the current YouTube video in y2mate, allowing you to download.
+// @version      1.1
+// @description  Pressing "CTRL+SHIFT+D" opens the current YouTube video in y2mate, allowing you to download.
 // @author       jijirae
 // @match        https://www.youtube.com/*
 // @grant        none
 // @icon         https://i.imgur.com/z8zguyi.png
 // ==/UserScript==
+
 (function() {
     'use strict';
 
     document.addEventListener('keydown', function(event) {
-        if (event.key === 'D') {
+        if (event.ctrlKey && event.shiftKey && event.key === 'D') {
             event.preventDefault();
             let currentUrl = window.location.href;
             let modifiedUrl = currentUrl.replace('youtube', 'youtubepp');
